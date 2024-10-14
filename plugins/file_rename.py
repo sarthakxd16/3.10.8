9259fa7cb14ab31942faa49b5796a3516d7a30aa
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-10-14 19:13:50 +0545

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -116,21 +116,21 @@ async def doc(bot, update):
     file_namr = mediac.file_name
 
     # file downloaded path
-    file_path = f"Renames/{new_filename}"
+    file_path = f"app/Renames/{new_filename}"
     
     # metadata downloaded path
-    metadata_path = f"Metadata/{new_filename}"
+    metadata_path = f"app/Metadata/{new_filename}"
 	
     ms = await update.message.edit("`Try To Download....`")    
     try:
         dl = f"downloads/{file_namr}"
-        dl_path = await bot.download_media(message=file, file_name=dl, progress=progress_for_pyrogram, progress_args=("ᴅᴏᴡɴʟᴏᴀᴅ sᴛᴀʀᴛᴇᴅ....", ms, time.time()))    
+        dl_path = await bot.download_media(message=file, progress=progress_for_pyrogram, progress_args=("ᴅᴏᴡɴʟᴏᴀᴅ sᴛᴀʀᴛᴇᴅ....", ms, time.time()))    
         print(dl_path)
     except Exception as e:
      	return await ms.edit(e)
     #os.rename(path, file_path)
     try:
-        os.rename(dl, file_path)
+        os.rename(dl_path, file_path)
     except Exception as e:
      	return await ms.edit(e)
     metadata_mode = await digital_botz.get_metadata_mode(user_id)
