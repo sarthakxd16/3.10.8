369de256e5524d8e1a1fe6c28acc0a4a7135aff9
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-10-15 15:08:13 +0545

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -25,7 +25,7 @@ app = Client("4gb_FileRenameBot", api_id=Config.API_ID, api_hash=Config.API_HASH
 
 async def metadata_editer(input_path, metadata, output_directory):
     output_path = f"{output_directory}/{time.time()}.mp4"
-    cmd = f"ffmpeg -i -map 0:v? -map 0:a? -map 0:s? {input_path} {metadata} -c:v copy -c:a copy -c:s copy {output_path} -y"
+    cmd = f"ffmpeg -i {input_path} {metadata} -map 0:v? -map 0:a? -map 0:s? -c:v copy -c:a copy -c:s copy {output_path} -y"
     
     process = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await process.communicate()
